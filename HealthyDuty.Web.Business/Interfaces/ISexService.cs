7ebc0430b1d34dc6635ab6b89513202a66321706
talha erdoğan <talha_erd_91@hotmail.com>
2021-03-24 18:16:49 +0300

    Add project files.

---
+++ HealthyDuty.Web.Business/Interfaces/ISexService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthyDuty.Web.Business.Interfaces
+{
+    public interface ISexService
+    {
+
+    }
+}
---
+++ HealthyDuty.Web/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthyDuty.Web.Controllers
+{
+    public class UserController : Controller
+    {
+       
+    }
+}
